5c4cfa25ecf67924c83259779724520a51ea56dc
Pasan <pasanu@protonmail.com>
2025-01-05 00:56:00 -0500

    get command

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
 
     # Wait for user input
     while True:
+        sys.stdout.write("$ ")
         user_input =  input()
         print(f"{user_input}: command not found\n") 
     
