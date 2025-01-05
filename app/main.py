2bb1ddb31075912a81416f8de8a956c5d32df219
Pasan <pasanu@protonmail.com>
2025-01-05 01:27:04 -0500

    refactored and added echo command

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 import sys
 
-
+COMMANDS = {
+    'exit': lambda code=0: exit(int(code)),
+    'echo': lambda *args: print(*args)
+}
 def main():
 
     # Wait for user input
     while True:
         sys.stdout.write("$ ")
-        user_input =  input()
-        if user_input == "exit 0":
-            exit(0)
-        print(f"{user_input}: command not found") 
+        user_input =  input().strip().split()
+
+        command, *args = user_input
+            
+        handler = COMMANDS.get(command)
+        if handler:
+            handler(*args)
+        else:
+            print(f"{user_input}: command not found") 
     
 
 
